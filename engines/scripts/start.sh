cdfbea166698f37746121ea8ec065ad3c18d6204
James Vodanovich <james@jvodan.com>
2015-01-02 22:49:47 +1100

    ini

--- engines/scripts/start.sh
+++ engines/scripts/start.sh
@@ -1,2 +1,2 @@
 cd /home/app/tiddly/wiki_name;
-node /usr/local/lib/node_modules/tiddlywiki/tiddlywiki.js editions/clientserver  --verbose  --server 8080 $:/core/save/all text/plain text/html uname pw 0.0.0.0;
+node /usr/local/lib/node_modules/tiddlywiki/tiddlywiki.js editions/clientserver  --verbose  --server 9001 $:/core/save/all text/plain text/html uname pw 0.0.0.0;
